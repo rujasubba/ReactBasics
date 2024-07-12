6f84f3357950bd7f3625377479520b7429b47551
RujaSubba <rujasubba123@gmail.com>
2024-07-12 20:55:23 +0930

    footer added

--- vite-React-basics-1/src/App.jsx
+++ vite-React-basics-1/src/App.jsx
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./styles/layout/hero.scss"
 import "./styles/layout/header.scss"
+import "./styles/layout/footer.scss"
 
 import Name from "./components/Name";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 import { Address } from "./components/Name";
 function App (){
 return(
@@ -15,6 +17,8 @@ return(
       <Route path="/Header" element={<Header />} />
       <Route path="/Name" element={<Name />} />
       <Route path="/Hero" element={<Hero />} />
+      <Route path="/Footer" element={<Footer />} />
+      
       
     </Routes>
   </React.Fragment>
